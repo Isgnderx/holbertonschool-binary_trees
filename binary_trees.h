3e405132403fb8e83cf28d1d4690826aded687a8
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-30 22:17:36 +0400

    a

--- binary_trees.h
+++ binary_trees.h
@@ -1,4 +1,26 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child
+ * @right: Pointer to the right child
+ */
+	typdef struct binary_tree_s{
+	
+	int data;
+	struct binary_tree_s parent;	
+	struct binary_tree_s *left, 
+	struct binary_tree_s *right;
+	
+	} binary_tree_t;
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+
 #endif /* BINARY_TREES_H */
