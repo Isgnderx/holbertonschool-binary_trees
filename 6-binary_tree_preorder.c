c234dbe8062bcfa38849de143aacd09ab28fbfc5
Isgnderx <ayxanisgenderzade999@gmail.com>
2026-01-31 19:49:09 +0400

    a

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -4,11 +4,11 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)	
 	{
 		return;
 	}
-	
-	binary_tree_preorder(tree->left);
-	binary_tree_preorder(tree->right);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
